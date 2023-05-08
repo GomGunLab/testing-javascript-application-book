cb9614c3932353e47462898edb54be21e7af99a8
Choi Hong Gyu <zxolqs@gmail.com>
2023-05-08 20:54:08 +0900

    chap07-01

---
+++ chap07/01/src/App.jsx
@@ -0,0 +1,12 @@
+import ReactDom from "react-dom";
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Inventory Contents V2</h1>
+    </div>
+  );
+};
+
+export default App;
